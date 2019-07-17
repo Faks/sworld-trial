e447743e07cdb2b8acb0a8082bbeb083074368b8
Faks <sia_dev@inbox.lv>
2019-07-17 20:08:16 +0300

    Eslint format

--- resources/js/components/HandleUploadComponent.vue
+++ resources/js/components/HandleUploadComponent.vue
@@ -3,9 +3,9 @@
         <div class="row" style="margin-bottom: 25px;">
             <div class="col-sm-4">
                 <div class="form-group">
-                    <label
-                        for="file"
-                        class="form-check-label">Please select new document:</label>
+                    <label for="file" class="form-check-label"
+                        >Please select new document:</label
+                    >
                     <input
                         name="pdf"
                         type="file"
@@ -13,47 +13,53 @@
                         ref="pdf"
                         accept="application/pdf"
                         v-on:change="onChangeFileUpload()"
-                        class="form-control"/>
+                        class="form-control"
+                    />
                 </div>
-                <button @click="handleFormSubmit" class="btn btn-primary">add new document</button>
+                <button @click="handleFormSubmit" class="btn btn-primary">
+                    add new document
+                </button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        props: ['api_token'],
-        data() {
-            return {
-                pdf: ''
-            }
-        },
-        methods: {
-            handleFormSubmit() {
-                let formData = new FormData();
-                formData.append('pdf', this.pdf);
-                
-                axios.post('api/v1/' + this.api_token + '/documents/store',
+export default {
+    props: ["api_token"],
+    data() {
+        return {
+            pdf: ""
+        };
+    },
+    methods: {
+        handleFormSubmit() {
+            let formData = new FormData();
+            formData.append("pdf", this.pdf);
+
+            axios
+                .post(
+                    "api/v1/" + this.api_token + "/documents/store",
                     formData,
                     {
                         headers: {
-                            'Content-Type': 'multipart/form-data'
+                            "Content-Type": "multipart/form-data"
                         }
                     }
-                ).then(function (data) {
-                        console.log(data.data);
-                        location.reload();
-                    })
-                    .catch(function () {
-                        console.log('FAILURE!!');
-                    });
-            },
-            
-            onChangeFileUpload() {
-                this.pdf = this.$refs.pdf.files[0];
-                console.log(this.pdf);
-            }
+                )
+                .then(function(data) {
+                    console.log(data.data);
+                    location.reload();
+                })
+                .catch(function() {
+                    console.log("FAILURE!!");
+                });
+        },
+
+        onChangeFileUpload() {
+            this.pdf = this.$refs.pdf.files[0];
+            console.log(this.pdf);
         }
     }
+};
 </script>
--- resources/js/components/ListingComponent.vue
+++ resources/js/components/ListingComponent.vue
@@ -1,73 +1,92 @@
 <template>
     <div>
         <div class="row">
-            <div v-for="list in getList" class="col-sm-4"
-                 style="margin-bottom: 25px;">
-                <div class="card" type="button"
-                     data-toggle="modal"
-                     v-bind:data-target="'#Modal'+ list.id +''">
-                    <img src="https://placeimg.com/285/200/nature" class="card-img-top" alt="...">
-                    
+            <div
+                v-for="list in getList"
+                class="col-sm-4"
+                style="margin-bottom: 25px;"
+            >
+                <div
+                    class="card"
+                    type="button"
+                    data-toggle="modal"
+                    v-bind:data-target="'#Modal' + list.id + ''"
+                >
+                    <img
+                        src="https://placeimg.com/285/200/nature"
+                        class="card-img-top"
+                        alt="..."
+                    />
+
                     <div class="card-body">
                         <h5 class="card-title">{{ list.file_name }}</h5>
                     </div>
-                    
+
                     <div class="card-body">
-                        <a v-bind:href="''+ list.file_path +'/' + list.file_name +''" class="card-link">Download</a>
+                        <a
+                            v-bind:href="
+                                '' + list.file_path + '/' + list.file_name + ''
+                            "
+                            class="card-link"
+                            >Download</a
+                        >
                     </div>
-                
                 </div>
             </div>
         </div>
-        
+
         <!-- The Modal -->
         <main v-for="list in getList">
             <!-- Modal -->
-            <div class="modal fade"
-                 v-bind:id="'Modal'+list.id +''"
-                 tabindex="-1" role="dialog"
-                 aria-labelledby="exampleModalLabel"
-                 aria-hidden="true">
-                <div
-                    class="modal-dialog modal-lg"
-                    role="document">
+            <div
+                class="modal fade"
+                v-bind:id="'Modal' + list.id + ''"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
-                        <object type="application/pdf"
-                                v-bind:data="''+ list.file_path +'/' + list.file_name +''"
-                                width="100%"
-                                height="100%"
-                                style="height: 85vh;">No Support
+                        <object
+                            type="application/pdf"
+                            v-bind:data="
+                                '' + list.file_path + '/' + list.file_name + ''
+                            "
+                            width="100%"
+                            height="100%"
+                            style="height: 85vh;"
+                            >No Support
                         </object>
                     </div>
                 </div>
             </div>
         </main>
-    
-    
     </div>
 </template>
 
 <script>
-    export default {
-        props: ['api_token'],
-        data() {
-            return {
-                getList: [],
-                errors: [],
-                fetch: false,
-            }
-        },
-        // Fetches posts when the component is created.
-        mounted() {
-            axios.get('api/v1/' + this.api_token + '/documents/list')
-                .then(response => {
-                    // JSON responses are automatically parsed.
-                    this.getList = response.data;
-                    this.fetch = true;
-                })
-                .catch(e => {
-                    this.errors.push(e)
-                })
-        }
+export default {
+    props: ["api_token"],
+    data() {
+        return {
+            getList: [],
+            errors: [],
+            fetch: false
+        };
+    },
+    // Fetches posts when the component is created.
+    mounted() {
+        axios
+            .get("api/v1/" + this.api_token + "/documents/list")
+            .then(response => {
+                // JSON responses are automatically parsed.
+                this.getList = response.data;
+                this.fetch = true;
+            })
+            .catch(e => {
+                this.errors.push(e);
+            });
     }
+};
 </script>
